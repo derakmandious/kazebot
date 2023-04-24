81e4e1ab409890bc8d5bb05e0a8c847ccf7d86dd
derakmandious <128228352+derakmandious@users.noreply.github.com>
2023-04-24 22:09:28 +0100

    changes to interactions

--- Kaze-bot.py
+++ Kaze-bot.py
@@ -1,6 +1,9 @@
 import discord
 from discord.ext import commands
-from discord_components import DiscordComponents, Button, ButtonStyle, Select, SelectOption
+from discord_slash import SlashCommand
+from discord_slash.utils.manage_commands import create_option, create_choice
+from discord_slash.model import ButtonStyle
+from discord_slash.utils.manage_components import create_button, create_actionrow, create_select, create_select_option
 import os
 import random
 from datetime import datetime, timedelta
